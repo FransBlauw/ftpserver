8449f0268b9eef5dc93878f037c997ca98a8e7e0
Frans Blauw <github@fransblauw.net>
2024-10-01 19:12:40 +0200

    Update for subdomains

--- createtls.sh
+++ createtls.sh
@@ -1,4 +1,4 @@
-DEFAULT_HOST="csb.fransblauw.rocks"
+DEFAULT_HOST="fransblauw.rocks"
 
 if [ -z "$1" ]; then
     echo "Using default host: $DEFAULT_HOST"
@@ -10,7 +10,7 @@ fi
 
 mkdir -p ./private/letsencrypt
 mkdir -p ./private/certificates
-certbot certonly --standalone --register-unsafely-without-email --agree-tos --non-interactive --config-dir ./private/letsencrypt/ --work-dir ./private/letsencrypt/ -d $HOST
+certbot certonly --preferred-challenges dns --manual --register-unsafely-without-email --agree-tos --config-dir ./private/letsencrypt/ --work-dir ./private/letsencrypt/ -d *.$HOST
 cp ./private/letsencrypt/live/$HOST/fullchain.pem ./private/certificates/fullchain.pem
 cp ./private/letsencrypt/live/$HOST/privkey.pem ./private/certificates/privkey.pem
 cat ./private/certificates/fullchain.pem ./private/certificates/privkey.pem > ./private/certificates/pure-ftpd.pem
