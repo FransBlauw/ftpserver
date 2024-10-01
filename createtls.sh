d31e3d9f487921862a2877edfc7205f9473a34f8
Frans Blauw <frans.blauw@hz.nl>
2024-10-01 12:44:12 +0200

    update

--- createtls.sh
+++ createtls.sh
@@ -1,5 +1,6 @@
 HOST=csb.fransblauw.rocks
 mkdir -p ./private/letsencrypt
+mkdir -p ./private/certificates
 certbot certonly --standalone --register-unsafely-without-email --agree-tos --non-interactive --config-dir ./private/letsencrypt/ --work-dir ./private/letsencrypt/ -d $HOST
 cp ./private/letsencrypt/live/$HOST/fullchain.pem ./private/certificates/fullchain.pem
 cp ./private/letsencrypt/live/$HOST/privkey.pem ./private/certificates/privkey.pem
