2c300997b5c12d333952ad38c23fdbb782f8d7fe
Frans Blauw <frans.blauw@hz.nl>
2024-10-01 09:43:40 +0200

    Update FTP stuff

--- ftp/entrypoint.sh
+++ ftp/entrypoint.sh
@@ -3,13 +3,29 @@
 
 USER_CSV=${FTP_USER_CSV:-/config/users.csv}
 
+PASSWD_FILE="/etc/pure-ftpd/passwd/pureftpd.passwd"
+PURE_DB="/etc/pure-ftpd/passwd/pureftpd.pdb"
+PURE_FTPD_FLAGS=""
+
+if [ -e $PASSWD_FILE ]
+then
+    echo "Password file already exists. Loading users..."
+    pure-pw mkdb "$PURE_DB" -f "$PASSWD_FILE"
+fi
+
+if [ -e /etc/ssl/private/pure-ftpd.pem ]
+then
+    echo "Enabling TLS..."
+    PURE_FTPD_FLAGS="$PURE_FTPD_FLAGS --tls=1 "
+fi
+
 if [ -f "$USER_CSV" ]; then
     echo "Processing users from $USER_CSV"
 
     # Skip the header and read each line
     tail -n +2 "$USER_CSV" | while IFS=, read -r username password
     do
-        pure-pw show "$username" &>/dev/null
+        pure-pw show "$username" -f "$PASSWD_FILE" -F "$PURE_DB" &>/dev/null
         userExists=$?
         if [ $userExists -eq 0 ]; then
             echo "User $username already exists. Skipping."
@@ -21,14 +37,14 @@ if [ -f "$USER_CSV" ]; then
             chown -R ftpuser:ftpgroup /home/ftpusers/$username
 
             # Create Pure-FTPd user
-            echo -e "$password\n$password" | pure-pw useradd $username -u ftpuser -d /home/ftpusers/$username/ -N 50 -m
+            echo -e "$password\n$password" | pure-pw useradd $username -u ftpuser -d /home/ftpusers/$username/ -N 50 -m -f "$PASSWD_FILE" -F "$PURE_DB"
             echo "Done."
         fi
     done
-
-    # Rebuild Pure-FTPd database
-    pure-pw mkdb
 fi
 
 # Start Pure-FTPd in foreground
-exec pure-ftpd -l puredb:/etc/pure-ftpd/pureftpd.pdb -E -j -R -p 30000:30009
+echo "Starting Pure-FTPd..."
+exec pure-ftpd -l puredb:$PURE_DB -E -j -R -p 30000:30009 $PURE_FTPD_FLAGS
+# ./run.sh -l puredb:/etc/pure-ftpd/pureftpd.pdb -E -j -R -P $PUBLICHOST
+
