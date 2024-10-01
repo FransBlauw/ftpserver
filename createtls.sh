c639e4f89ba92df3ede224313aad7459d9752b20
Frans Blauw <frans.blauw@hz.nl>
2024-10-01 11:33:30 +0200

    update

--- createtls.sh
+++ createtls.sh
@@ -3,4 +3,4 @@ mkdir -p ./private/letsencrypt
 certbot certonly --standalone --register-unsafely-without-email --agree-tos --non-interactive --config-dir ./private/letsencrypt/ --work-dir ./private/letsencrypt/ -d $HOST
 cp ./private/letsencrypt/live/$HOST/fullchain.pem ./private/certificates/fullchain.pem
 cp ./private/letsencrypt/live/$HOST/privkey.pem ./private/certificates/privkey.pem
-cat ./private/certificates/fullchain.pem ./private/certificates/privkey.pem > ./private/pure-ftpd.pem
+cat ./private/certificates/fullchain.pem ./private/certificates/privkey.pem > ./private/certificates/pure-ftpd.pem
---
+++ prepare.sh
@@ -0,0 +1,2 @@
+apt install docker docker-compose git certbot 
+git config --global credential.helper cache
