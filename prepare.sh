3686a321cf0fe0041ab1ded8743b03e4f7e0a63e
Frans Blauw <frans.blauw@hz.nl>
2024-10-01 13:22:35 +0200

    update

--- prepare.sh
+++ prepare.sh
@@ -1,2 +1,16 @@
-apt install docker docker-compose git certbot 
-git config --global credential.helper cache
+# From https://docs.docker.com/engine/install/debian/
+sudo apt-get update
+sudo apt-get -y install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+
+apt install -y git certbot docker-ce docker-ce-cli docker-compose-plugin
+git config --global credential.helper store
